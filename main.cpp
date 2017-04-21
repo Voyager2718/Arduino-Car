5efba210deac8debe08b17cb7a4104b0a6bcb548
Voyager2718 <Voyager2718@users.noreply.github.com>
2017-04-21 12:51:41 +0800

    Deleted non-sense.

--- main.cpp
+++ main.cpp
@@ -7,15 +7,7 @@
 using std::cout;
 using std::endl;
 
-std::vector<int> make_vector(){
-    std::vector<int> v;
-    return v;
-}
-
 int main(){
-    std::map< std::string, std::vector<int> > map;
-    map["a"] = make_vector();
-    map["a"].push_back(10);
-    cout<<map["a"][0]<<endl;
+    
     return 0;
 }
